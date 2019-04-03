97e9b63b6db2f019043b8645eae240bc44b8f163
bappyasif <bappyasif@hotmail.com>
2019-04-03 21:23:14 +0600

    First Katalon Studio project Commit.

---
+++ Test Suites/TryingAnalytics/New Test.ts
@@ -0,0 +1,37 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>c580015f-aed7-407c-8cb1-cac860d860e5</testSuiteGuid>
+   <testCaseLink>
+      <guid>9ce177ba-f5a5-4ee7-8235-ca17a0ba49c4</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTHTTPMethodsTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46a5f163-e80f-46c8-8823-ea826dbea9cd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTListUsersTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dc9b6c95-1e8b-4d09-9df8-45f38f32955e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/CalculatorTests/AditionSOAP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>eaf4701c-4e56-46fd-8c8d-d637a75363b7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/GetBankDetailsTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/TryingCMD/Console Mode.ts
@@ -0,0 +1,39 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>How to run API tests from command line
+
+https://docs.katalon.com/katalon-studio/docs/console-mode-execution.html#use-consoleproperties-file</description>
+   <name>Console Mode</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>a7ccc3a2-94f6-4e08-9666-56eba4d10951</testSuiteGuid>
+   <testCaseLink>
+      <guid>f54f0524-fa30-4251-9e9e-578308044ae1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/CalculatorTests/AditionSOAP</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0866a7e3-59aa-4c84-b566-c1ffc31600e1</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/GetBankDetailsTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>44887664-e8ef-4e2f-babf-5e5a38e951f7</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RESTHTTPMethodsTest</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>43145acc-29ab-45d9-b5b5-e3ce32ce66ce</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>false</isRun>
+      <testCaseId>Test Cases/RESTListUsersTest</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
